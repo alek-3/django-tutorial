4b9ba8206fb1f179b9578df67edc9ac56f6b373e
alek-3 <jaianism.of.grotius@hotmail.co.jp>
2017-12-03 00:11:45 +0900

    バグ修正・テスト追加

--- polls/tests.py
+++ polls/tests.py
@@ -7,9 +7,20 @@ from .models import Question
 
 
 class QuestionModelTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         """未来日付に追加された質問を排除する"""
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """過去日付の質問をFalseを返す"""
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        past_question = Question(pub_date=time)
+        self.assertIs(past_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """一日以内に追加された質問についてTrueを返す"""
+        time =timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
